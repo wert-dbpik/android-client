b417751b12b35ab31d87a5236d4ad3a9ac9a98f5
Aleksei V. Simonov <wert001@yandex.ru>
2023-05-19 16:00:45 +0300

    Create ChatConnection.java

---
+++ app/src/main/java/ru/wert/bazapik_mobile/chat/websocket/ChatConnection.java
@@ -0,0 +1,17 @@
+package ru.wert.bazapik_mobile.chat.websocket;
+
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
+
+import static ru.wert.bazapik_mobile.ThisApplication.DATA_BASE_URL;
+
+public class ChatConnection {
+
+    private static final String TAG = "ChatConnection";
+
+    private StompClient mStompClient;
+
+    public ChatConnection() {
+        mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://" + DATA_BASE_URL + "/example-endpoint/websocket");
+    }
+}
